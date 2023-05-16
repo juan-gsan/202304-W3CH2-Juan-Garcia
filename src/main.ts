15362083b9040082662e01d741173ec552749e7b
JuanG <juan.gsanchez13@gmail.com>
2023-05-16 17:56:36 +0200

    Add main structure

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,42 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import './style.css';
+import { createCharacters } from './data/createCharacters.ts';
+import { createTemplate } from './template.ts';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const data = createCharacters();
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+let htmlList = '';
+
+data.forEach((character) => {
+  htmlList += createTemplate(character);
+});
+
+document.querySelector('.app')!.innerHTML = `
+
+    <ul class="characters-list row list-unstyled">
+      ${htmlList}
+    </ul>`;
+
+const handleDie = (element: HTMLButtonElement) => {
+  data.find((item) => item.name === element.dataset.id)!.endLife();
+};
+
+const handleSpeak = (element: HTMLButtonElement) => {
+  data.find((item) => item.name === element.dataset.id)!.saySentence();
+};
+
+const handleButtons = (event: Event) => {
+  const element = event.target as HTMLButtonElement;
+  if (element.classList.contains('die')) {
+    handleDie(element);
+  }
+
+  if (element.classList.contains('speak')) {
+    handleSpeak(element);
+  }
+};
+
+const buttons =
+  document.querySelectorAll<HTMLButtonElement>('.character__action');
+buttons.forEach((button) => {
+  button.addEventListener('click', handleButtons);
+});
